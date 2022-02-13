7483ff6f8740685cd44dd02ede63748057570c97
e.nakagawa <t-square.ewi4000s@erisaku.sakura.ne.jp>
2022-02-14 05:39:14 +0900

    対象アイテム固定状態

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "chota";
+  import CardList from "./CardListComponent.svelte";
+
+  let items = [...Array(8).keys()].map((i) => {
+    let j = i + 1;
+    return {
+      id: [i],
+      name: ["item" + j],
+      cost: j,
+      createCount: 0,
+      createCost: 0,
+      mergeGrid: 1,
+      columnStart: 0,
+      columnEnd: 0,
+    };
+  });
+</script>
+
+<main>
+  <CardList {items} />
+</main>
+
+<style>
+  main {
+    margin: 2em 0;
+  }
+</style>
